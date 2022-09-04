03297209bbf7da29d97694ac64f37a32e4190895
singhbipinb <bipinsingh130100@gmail.com>
2022-09-04 19:00:39 +0530

    my Library

--- myLibrary/__init__.py
+++ myLibrary/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, make_response
 from config import Config
 from flask_pymongo import PyMongo
 
@@ -6,6 +6,11 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 app.config.from_object(Config)
 mongo = PyMongo(app)
+
+
+@app.route("")
+def welcome():
+    return make_response("Welcome to myLibrary")
 
 from myLibrary.books import books as books
 from myLibrary.transactions import transaction
